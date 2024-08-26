0e25c40cac8daad1707968060c2d876fb2221b1e
anka <1350989414@qq.com>
2024-08-26 18:59:03 +0800

    基本是最终版本

--- commercecorevue/vue.config.js
+++ commercecorevue/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/CommerceCore/" : "/",
   outputDir: "dist",
 });
